c9d7ed4569c312fcb5fbf9722f8ef01ed3888291
imperio29 <125302401+imperio29@users.noreply.github.com>
2023-02-13 12:19:48 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Declare variables below to save the different sections (divs) of your story*/
+//first button option
+let acceptButton = document.querySelector(".option-one");
+//first option screen
+let acceptDiv = document.querySelector(".option-one-screen");
+let accecptButtion= document.querySelector(".gray briefcases");
+//second button option
+let declineButton = document.querySelector(".option-two");
+//second option screen
+let declineDiv = document.querySelector(".option-two-screen");
+let declineImg= document.querySelector(".uber");
+//first end option
+let moneyScreen= document.querySelector("option-one-end");
+let moneyButton= document.querySelector(".rats");
+let greenButton=document.querySelector(".money");
+//second end option
+let uberScreen=document.querySelector(".option-two-end");
+let body=document.querySelector("body");
+
+//* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+acceptButton.onclick=function(){
+acceptDiv.style.displays="block"; 
+body.style="background-color:#000000"; 
+
+};
+
+
+
+
